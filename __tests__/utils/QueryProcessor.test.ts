7982c9d053d1089d178f1b04821a3fb2083588f7
Kevin He <127244212+KevinHeeHee@users.noreply.github.com>
2024-03-20 00:40:25 -0400

    change test file

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -15,7 +15,7 @@ describe("QueryProcessor", () => {
             "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
             "English poet, playwright, and actor, widely regarded as the greatest " +
             "writer in the English language and the world's pre-eminent dramatist."
-          ));
+        ));
     });
 
     test('should return andrew id', () => {
@@ -23,6 +23,6 @@ describe("QueryProcessor", () => {
         const response: string = QueryProcessor(query);
         expect(response).toBe((
             "kevinhe"
-          ));
+        ));
     });
 });
